21b088e4457e31fe6f4e304d3df9665e2752d196
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2022-12-04 14:38:14 -0500

    Add clear form functionality

--- widgets/ipForm.go
+++ widgets/ipForm.go
@@ -10,9 +10,19 @@ func initIPForm() {
 	IPForm.SetTitle("Enter the required data").SetTitleAlign(tview.AlignLeft)
 	IPForm.AddInputField("Host IP address", "", 30, nil, nil)
 	IPForm.AddInputField("Netmask", "", 30, nil, nil)
-	IPForm.AddButton("Get Values", nil)
-	IPForm.AddButton("Clear", nil)
+	IPForm.AddButton("Get Values", getValues)
+	IPForm.AddButton("Clear", clearIPForm)
 	IPForm.SetItemPadding(2)
 	IPForm.SetBorderPadding(1, 1, 3, 3)
 
+}
+
+func getValues(){
+
+}
+
+func clearIPForm(){
+	IPForm.Clear(false)
+	IPForm.AddInputField("Host IP address", "", 30, nil, nil)
+	IPForm.AddInputField("Netmask", "", 30, nil, nil)
 }
