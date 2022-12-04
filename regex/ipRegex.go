857e75723c4b649e1bcb4138a96d91827a0acad0
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2022-12-04 14:09:09 -0500

    Improve regular expressions

--- regex/ipRegex.go
+++ regex/ipRegex.go
@@ -10,5 +10,13 @@ func init(){
 	ipv4Regex = regexp.MustCompile(`^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}$`)
 
 	//https://stackoverflow.com/questions/5360768/regular-expression-for-subnet-masking
-	ipv4MaskRegex = regexp.MustCompile(`((255|254|252|248|240|224|192|128).){3}(255|254|252|248|240|224|192|128)`)
+	ipv4MaskRegex = regexp.MustCompile(`^((255|254|252|248|240|224|192|128).){3}(255|254|252|248|240|224|192|128)$`)
+}
+
+func CheckIpv4(value string) bool{
+	return ipv4Regex.Match([]byte(value))
+}
+
+func CheckIpv4Mask(value string) bool {
+	return ipv4MaskRegex.Match([]byte(value))
 }
