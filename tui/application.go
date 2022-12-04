862de3dc2245d784b91d4c1a3f549381ebc5c5d1
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2022-12-03 19:24:17 -0500

    Finish initial TUI structure

--- tui/application.go
+++ tui/application.go
@@ -14,6 +14,14 @@ var pages *tview.Pages
 var quitModal *tview.Modal
 var grid *tview.Grid
 var ipForm *tview.Form
+var resultsGrid *tview.Grid
+var networkAddress *tview.TextView
+var broadcastAddress *tview.TextView
+var usableRange *tview.TextView
+var totalIPAddresses *tview.TextView
+var usableIPAddresses *tview.TextView
+var ipType *tview.TextView
+var clearResultsButton *tview.Form
 
 func init(){
 
@@ -28,13 +36,59 @@ func init(){
 	ipForm.AddInputField("Netmask","",30,nil,nil)
 	ipForm.AddButton("Get Values",nil)
 	ipForm.AddButton("Clear",nil)
+	ipForm.SetItemPadding(2)
+	ipForm.SetBorderPadding(1,1,3,3)
+
+	networkAddress = tview.NewTextView()
+	broadcastAddress = tview.NewTextView()
+	usableRange = tview.NewTextView()
+	totalIPAddresses = tview.NewTextView()
+	usableIPAddresses = tview.NewTextView()
+	ipType = tview.NewTextView()
+
+	clearResultsButton = tview.NewForm().
+		AddButton("Clear Results",nil).
+		SetButtonsAlign(tview.AlignRight)
+	
+	clearResultsButton.SetBorderPadding(0,0,0,0)
+	clearResultsButton.SetItemPadding(0)
+
+	resultsGrid = tview.NewGrid().
+		SetColumns(0,0).
+		SetRows(0,0,0,0,0,0,0).
+		SetBorders(false).
+		SetMinSize(2,2).
+		AddItem(tview.NewTextView().SetText("Network Address"),0,0,1,1,1,0,false).
+		AddItem(tview.NewTextView().SetText("Broadcast Address"),1,0,1,1,1,0,false).
+		AddItem(tview.NewTextView().SetText("Usable Rage"),2,0,1,1,1,0,false).
+		AddItem(tview.NewTextView().SetText("Total IP addresses"),3,0,1,1,1,0,false).
+		AddItem(tview.NewTextView().SetText("Usable IP addresses"),4,0,1,1,1,0,false).
+		AddItem(tview.NewTextView().SetText("IP type"),5,0,1,1,1,0,false).
+		AddItem(networkAddress,0,1,1,1,1,0,false).
+		AddItem(broadcastAddress,1,1,1,1,1,0,false).
+		AddItem(usableRange,2,1,1,1,1,0,false).
+		AddItem(totalIPAddresses,3,1,1,1,1,0,false).
+		AddItem(usableIPAddresses,4,1,1,1,1,0,false).
+		AddItem(ipType,5,1,1,1,1,0,false).
+		AddItem(clearResultsButton,6,1,1,1,1,0,false)
+
+
+
+		resultsGrid.SetTitle("Results")
+		resultsGrid.SetBorder(true)
+		resultsGrid.SetBorderPadding(1,1,2,2)
+		resultsGrid.SetTitleAlign(tview.AlignCenter)
+	
 
 	grid = tview.NewGrid().
 		SetColumns(0,0).
 		SetRows(2,0,1).
-		SetBorders(true).
+		SetBorders(false).
 		AddItem(title,0,0,1,2,0,0,true).		
-		AddItem(ipForm,1,0,1,1,0,0,false)
+		AddItem(ipForm,1,0,1,1,0,0,false).
+		AddItem(resultsGrid,1,1,1,1,0,0,false)
+
+	grid.SetBorderPadding(0,0,2,2)
 
 	quitModal = tview.NewModal().SetText("Do you really want to quit?").
 		AddButtons([]string {"Yes","No"}).
